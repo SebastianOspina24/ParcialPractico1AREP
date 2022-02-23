63aacaacf302b200a100e7b617ed901eae505597
unknown <juan.ospina-c@labinfo.is.escuelaing.edu.co>
2022-02-23 14:41:21 -0500

    Funciona la respuesta de la pagina web quemada pero no lo de el api

--- src/main/java/edu/escuelaing/arep/returns/Clima.java
+++ src/main/java/edu/escuelaing/arep/returns/Clima.java
@@ -1,9 +1,20 @@
 package edu.escuelaing.arep.returns;
 import java.io.IOException;
-import java.net.Socket;
 
 public class Clima {
-    public static void getInstance(Socket clienteSocket)throws IOException{
-        
+    public static String getInstance()throws IOException{
+
+        String outputLine = 
+          "<!DOCTYPE html>" + 
+          "<html>" + 
+          "<head>" + 
+          "<meta charset=\"UTF-8\">" + 
+          "<title>API Clima ECI</title>\n" + 
+          "</head>" + 
+          "<body>" + 
+          "<h1>En esta api se puede consultar el clima de diferentes ciudades con solo usar su nombre</h1>" + 
+          "</body>" + 
+          "</html>"; 
+        return outputLine;
     }
 }
--- src/main/java/edu/escuelaing/arep/returns/Response.java
+++ src/main/java/edu/escuelaing/arep/returns/Response.java
@@ -1,31 +1,61 @@
 package edu.escuelaing.arep.returns;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.URL;
 
 public class Response {
 
-    public static void response(Socket clienteSocket,String entrada) {
+    public static void response(Socket clienteSocket, String entrada) {
         String url;
         if (entrada.contains("GET")) {
             url = entrada.split(" ")[1].substring(1);
+            //System.out.println(url);
             try {
-                if(url.contains("/clima")){
-                    Clima.getInstance(clienteSocket);
-                }
-                else if(url.contains("7consulta")){
-
-                }
-                else{
+                if (url.contains("clima")) {
+                    printing(clienteSocket,Clima.getInstance());
+                } else if (url.contains("consulta?lugar=")) {
+                    consulta(clienteSocket,url);
+                } else {
 
                 }
             } catch (IOException e) {
-                
+                System.out.println(e.getMessage());
             }
         }
     }
+
+private static void printing(Socket clienteSocket,String s) throws IOException{
+    System.out.println("Se envia respuesta");
+    PrintWriter out = new PrintWriter(
+        clienteSocket.getOutputStream(), true);
+        s = "HTTP/1.1 200 OK\r\n Content-Type: text/html \r\n\r\n"+s;
+    out.println(s);
+    out.close(); 
+}
+
+private static void consulta(Socket clienteSocket,String url) throws IOException{
+    String ciudad = url.split("=")[1];
+    String apiconection = "https://api.openweathermap.org/data/2.5/weather?q=" + ciudad
+            + "&appid=d60de446da476e3e141cd457145c6f19";
+    URL obj = new URL(apiconection);
+    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+    con.setRequestMethod("GET");
+    con.getResponseCode();
+    PrintWriter out = new PrintWriter(
+        clienteSocket.getOutputStream(), true);
+        BufferedInputStream temp = new BufferedInputStream(con.getInputStream());
+    System.out.print(temp);
+    out.print("HTTP/1.1 200 OK\r\n Content-Type: text/json \r\n\r\n");
+    out.print(temp);
+    out.close();
+}
 
     public static void getentry(Socket clienteSocket) throws IOException {
         BufferedReader in = new BufferedReader(
@@ -37,6 +67,8 @@ public class Response {
                 break;
             }
         }
+        in.close();
+        clienteSocket.close();
     }
 
 }
