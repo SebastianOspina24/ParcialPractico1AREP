1e6d57fa92cff6fdbef0c4c4de6e4caf51566e21
unknown <juan.ospina-c@labinfo.is.escuelaing.edu.co>
2022-02-23 15:45:38 -0500

    Falta arreglar el error de js

--- src/main/java/edu/escuelaing/arep/returns/Clima.java
+++ src/main/java/edu/escuelaing/arep/returns/Clima.java
@@ -1,31 +1,60 @@
 package edu.escuelaing.arep.returns;
+
 import java.io.IOException;
 
 public class Clima {
-    public static String getInstance()throws IOException{
+    public static String getInstance() throws IOException {
 
-        String outputLine = 
-          "<!DOCTYPE html>" + 
-          getCoder()+
-          "<html>" + 
-          "<head>" + 
-          "<meta charset=\"UTF-8\">" + 
-          "<title>API Clima ECI</title>\n" + 
-          "</head>" + 
-          "<body>" + 
-          "<h1>En esta api se puede consultar el clima de diferentes ciudades con solo usar su nombre</h1>" + 
-          "</body>" + 
-          "</html>"; 
+        String outputLine = "<!DOCTYPE html>" +
+                getCoder() +
+                "<html>" +
+                "<head>" +
+                "<meta charset=\"UTF-8\">" +
+                "<title>API Clima ECI</title>\n" +
+                "</head>" +
+                "<body>" +
+                "<h1>En esta api se puede consultar el clima de diferentes ciudades con solo usar su nombre</h1>" +
+                "<section id=\"Test\" class=\"test-section\">" +
+                "<div class=\"section-header\">" +
+                "<h2>Testing</h2>" +
+                "</div>" +
+                "<div class=\"testing\">" +
+                "<center>" +
+                "<h2>Selecciona la unidad a convertir:</h2>" +
+                "</select>" +
+                "</center>" +
+                "</div>" +
+                "<div class=\"testing\">" +
+                "<input class=\"box\" type=\"Text\" id=\"nombre\" placeholder=\"Nombre de la Ciudad \">" +
+                "</div>" +
+                "<div class=\"testing\">" +
+                "<button  class=\"box\" onclick=\"consumirapi()\" class=\"btn\">Consultar</button>" +
+                "</div>" +
+                "<center>" +
+                "<div class=\"testing\">" +
+                "<h3>Nombre</h3>" +
+                "<p id=\"city\"></p>" +
+                "</div>" +
+                "</center>" +
+                "<center>" +
+                "<div class=\"testing\">" +
+                "<h3>Temperatura</h3>" +
+                "<p id=\"tempe\"></p>" +
+                "</div>" +
+                "</center>" +
+                "</section>" +
+                "</body>" +
+                "</html>";
         return outputLine;
     }
 
-    public static String getCoder(){
-        return "<script>let url = \"https://parcialarepno1.herokuapp.com/consulta?lugar=\""+
-        "function consumirapi(){"+
-        "fetch(url+document.getElementById(\"valor\").value"+
-       "  .then(response => response.json())"+
-          ".then(data => {"+
-            "document.getElementById(\"city\").innerHTML = data.name"+
-            "document.getElementById(\"tempe\").innerHTML = data.temp})}</script>";
+    public static String getCoder() {
+        return "<script type=\"text/javascript\">let url = \"https://parcialarepno1.herokuapp.com/consulta?lugar=\"\n" +
+                "function consumirapi(){\n" +
+                "fetch(url+document.getElementById(\"nombre\")\n" +
+                "  .then(response => response.json())\n" +
+                ".then(data => {\n" +
+                "document.getElementById(\"city\").innerHTML = data.name\n" +
+                "document.getElementById(\"tempe\").innerHTML = data.temp\n}))\n}\n</script>\n";
     }
 }
